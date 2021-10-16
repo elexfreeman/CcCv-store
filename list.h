f8bb58f66e23a33523ef266203369d1530b012c0
elexfreeman <elextraza@gmail.com>
2021-10-16 14:37:26 +0300

    INIT

---
+++ list.h
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct stru_list
+{
+  int data;
+  struct stru_list *next;
+};
+
+
+void printList(struct stru_list * head)
+{
+  struct stru_list *ptr = head;
+  printf("n[head] =>");
+  while(ptr != NULL)
+  {
+    printf(" %d =>",ptr->data);
+    ptr = ptr->next;
+  }
+  printf(" [null]n");
+}
+
+struct stru_list * insert(struct stru_list * head, int data)
+{
+  struct stru_list *link = (struct stru_list*) malloc(sizeof(struct stru_list));
+  link->data = data;
+  link->next = head;
+  return link;
+}
+
+int get(struct stru_list * head, int key)
+{
+  int resp = 0;
+  int n = 0;
+  struct stru_list *ptr = head;
+  while(ptr != NULL)
+  {
+    if(n==key)
+    {
+      resp = ptr->data;
+      ptr = NULL;
+    }
+    else 
+    {
+      ptr = ptr->next;
+    }
+    n++;
+  }
+  return resp;
+
+}
