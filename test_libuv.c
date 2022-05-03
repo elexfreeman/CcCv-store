7c96b1dc0f5c6226d09b0c328137dd962a31d45d
Alexander Polozov <polozov@alkor.co.ru>
2022-05-03 15:47:28 +0400

    someo

--- test_libuv.c
+++ test_libuv.c
@@ -13,7 +13,6 @@
 #include "thread_disc_sync.c"
 #include "thread_task_manager.c"
 
-#define DEFAULT_PORT 7000
 #define DEFAULT_BACKLOG 128
 
 uv_loop_t *loop;
@@ -35,7 +34,10 @@ void alloc_buffer(uv_handle_t *handle, size_t suggested_size, uv_buf_t *buf) {
   buf->len = suggested_size;
 }
 
-void on_close(uv_handle_t *handle) { free(handle); }
+void on_close(uv_handle_t *handle) {
+  fprintf(stdout, "on_close \r\n");
+  free(handle);
+}
 
 void echo_write(uv_write_t *req, int status) {
   if (status) {
@@ -53,8 +55,10 @@ void echo_read(uv_stream_t *client, ssize_t nread, const uv_buf_t *buf) {
     return;
   }
   if (nread < 0) {
-    if (nread != UV_EOF)
+    if (nread != UV_EOF) {
       fprintf(stderr, "Read error %s\n", uv_err_name(nread));
+    }
+    fprintf(stdout, "end read \r\n");
     uv_close((uv_handle_t *)client, on_close);
   }
 
@@ -68,16 +72,35 @@ void on_new_connection(uv_stream_t *server, int status) {
     return;
   }
 
+  fprintf(stdout, "New client connected \r\n");
   uv_tcp_t *client = (uv_tcp_t *)malloc(sizeof(uv_tcp_t));
   uv_tcp_init(loop, client);
   if (uv_accept(server, (uv_stream_t *)client) == 0) {
     uv_read_start((uv_stream_t *)client, alloc_buffer, echo_read);
   } else {
+    fprintf(stdout, "client exit");
     uv_close((uv_handle_t *)client, on_close);
   }
 }
 
+void app_finish() {
+  struct stru_config *config = get_config();
+  free(config);
+  fprintf(stdout, "App exit...\n");
+  abort();
+}
+
+void sig_handler(int sig) {
+  switch (sig) {
+  case SIGSEGV:
+    app_finish();
+  default:
+    app_finish();
+  }
+}
+
 int main() {
+  signal(SIGSEGV, sig_handler);
   if (init_config() == 0) {
     fprintf(stdout, "ERROR: bad config file r\n");
     return 1;
@@ -86,15 +109,16 @@ int main() {
   struct stru_config *config = get_config();
   if (config == NULL) {
     fprintf(stdout, "Empty config r\n");
+    return 1;
   }
+
   fprintf(stdout, "server start at 0.0.0.0:%d r\n", config->port);
-  return 0;
   loop = uv_default_loop();
 
   uv_tcp_t server;
   uv_tcp_init(loop, &server);
 
-  uv_ip4_addr("0.0.0.0", DEFAULT_PORT, &addr);
+  uv_ip4_addr("0.0.0.0", config->port, &addr);
 
   uv_tcp_bind(&server, (const struct sockaddr *)&addr, 0);
   int r = uv_listen((uv_stream_t *)&server, DEFAULT_BACKLOG, on_new_connection);
